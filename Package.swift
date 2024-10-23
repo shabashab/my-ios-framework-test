05eb84d65af0532e47023570a29591ef092da18d
Artem Tarasenko <shabashab.04@gmail.com>
2024-10-23 16:57:56 +0300

    init project

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "my-framework-test",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "my-framework-test",
+            targets: ["my-framework-test"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "my-framework-test"),
+        .testTarget(
+            name: "my-framework-testTests",
+            dependencies: ["my-framework-test"]),
+    ]
+)
---
+++ Sources/my-framework-test/my_framework_test.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class MyFrameworkClass {
+    public init() {}
+    
+    public func foo() {
+        print("Foo!")
+    }
+}
